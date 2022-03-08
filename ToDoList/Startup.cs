3cab72d1c5f66032c3e3e236ee227794a45e04db
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-03-08 08:09:00 -0800

    update Startup.cs

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -3,26 +3,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 namespace ToDoList
 {
   public class Startup
   {
     public Startup(IWebHostEnvironment env)
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
+      var builder = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json");
       Configuration = builder.Build();
     }
 
-    public IConfigurationRoot Configuration { get; }
+    public IConfigurationRoot Configuration { get; set; }
 
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
-    }
 
+      services.AddEntityFrameworkMySql()
+          .AddDbContext<ToDoListContext>(options => options
+          .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+    }
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
@@ -39,9 +41,5 @@ namespace ToDoList
         await context.Response.WriteAsync("Hello World!");
       });
     }
-  }
-  public static class DBConfiguration
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
   }
 }
